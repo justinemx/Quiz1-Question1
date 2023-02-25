7cf56f48d1b7a262eb07e7587e0326f5cef19a7c
Justine Manalansan <jstnmnlnsn@gmail.com>
2023-02-25 19:25:09 +0800

    Elect2-Activity-0213

--- Quiz1/Item-2/Product.php
+++ Quiz1/Item-2/Product.php
@@ -1,14 +1,84 @@
 <?php
 
-  class Product {
+  abstract class Product {
     protected $productName;
     protected $productPrice;
+
+    /**
+     * @param $productName
+     * @param $productPrice
+     */
     public function __construct($productName, $productPrice) {
-      $this->name = $productName;
-      $this->price = $productPrice;
+      $this->productName = $productName;
+      $this->productPrice = $productPrice;
     }
-    public function display() {
-      echo "<p><strong>Product Name:</strong> {$this->productName}</p>";
-      echo "<p><strong>Price:</strong> {$this->productPrice}</p>";
+
+    abstract function display();
+
+    /**
+     * @return mixed
+     */
+    public function getProductName() {
+      return $this->productName;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getProductPrice() {
+      return $this->productPrice;
+    }
+
   }
+
+  class Movie extends Product {
+    protected $director;
+    protected $rating;
+
+    public function __construct($productName, $productPrice, $director, $rating) {
+      parent::__construct($productName, $productPrice);
+      $this->director = $director;
+      $this->rating = $rating;
+    }
+
+    function display() {
+      return "Name: " . $this->productName . "<br>Price: " . $this->productPrice . "<br>Director: " . $this->director . "<br>Rating: " . $this->rating;
+    }
+
+    public function getDirector() {
+      return $this->director;
+    }
+
+    public function getRating() {
+      return $this->rating;
+    }
+
+  }
+
+  class Book extends Product {
+    protected $author;
+    protected $genre;
+
+    public function __construct($productName, $productPrice, $author, $genre) {
+      parent::__construct($productName, $productPrice);
+      $this->author = $author;
+      $this->genre = $genre;
+    }
+
+    function display() {
+      return "Name: ".$this->productName . "<br>Price: ".$this->productPrice . "<br>Author: ".$this->author. "<br>Genre: ".$this->genre;
+    }
+
+    public function getAuthor() {
+      return $this->author;
+    }
+
+    public function getGenre() {
+      return $this->genre;
+    }
+
+  }
+
+
+
+
--- Quiz1/Item-2/index.php
+++ Quiz1/Item-2/index.php
@@ -1,49 +1,67 @@
 <?php
 
-  require_once 'Product.php';
-  use Product\Movie;
-  use Product\Book;
+    require_once "Product.php";
 
-  $msg = "";
+    $product = null ;
 
-  switch($_POST['selectedOption']){
-    case 'Book':
-      $book = new Book();
-      $book->display();
-      break;
-    case 'Movie':
-      $movie = new Movie();
-      $movie->display();
-      break;
-    default:
-      // Something went wrong or form has been tampered.
-  }
+    if (isset($_POST['submit'])) {
+      if ( isset($_POST['productType']) && isset($_POST['title']) && isset($_POST['price']) && isset($_POST['creator']) && isset($_POST['genreRating'])) {
+        $productType = $_POST['productType'];
+        $title = $_POST['title'];
+        $price = $_POST['price'];
+        $creator = $_POST['creator'];
+        $genreRating = $_POST['genreRating'];
 
-?>
+        $product = $productType == "Movie" ?
+          new Movie($title,$price, $creator, $genreRating) : new Book( $title, $price, $creator, $genreRating );
+      }
+    }
+
+  ?>
 
   <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  </head>
-  <body>
-    <div class="d-flex align-items-center justify-content-center vh-100" style="flex-direction: column;">
-      <form method="POST">
-        <label>Choose between a book or a movie :
-            <select class="form-select" name="selectedOption">
-              <option value="Book">Harry Potter and the Philosopher's Stone (Book)</option>
-              <option value="Movie">The Shawshank Redemption (Movie)</option>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Title</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </head>
+    <body>
+      <div class="d-flex align-items-center justify-content-center vh-100" style="flex-direction: column;">
+        <form method="POST">
+          <div class="mb-3">
+            <label for="input-producttype">Product type</label>
+            <select id="input-producttype" class="form-select" name="productType">
+              <option value="Book">Book</option>
+              <option value="Movie">Movie</option>
             </select>
-          <div style="text-align: center; margin-top: 10px; margin-bottom: 10px">
-            <input name="Submit" value="Submit" type="Submit" class="btn btn-primary"/>
           </div>
-        </label>
-      </form>
-      <div style="text-align: center">
-        <?php echo '<br/>' . $msg; ?>
+          <div class="mb-3">
+            <label for="input-name" class="form-label" id="input-length-label">Title</label>
+            <input id="input-name" class="form-control" type="text" name="title" required>
+          </div>
+          <div class="mb-3">
+            <label for="input-price" class="form-label" id="input-length-label">Price</label>
+            <input id="input-price" class="form-control" type="text" name="price"
+                   required>
+          </div>
+          <div class="mb-3">
+            <label for="input-creator" class="form-label" id="input-length-label">Author or Director</label>
+            <input id="input-creator" class="form-control" type="text" name="creator"
+              required>
+          </div>
+          <div class="mb-3">
+            <label for="input-genrerating" class="form-label" id="input-length-label">Genre or Rating</label>
+            <input id="input-genrerating" class="form-control" type="text" name="genreRating" required>
+          </div>
+          <div style="text-align: center; margin-top: 10px; margin-bottom: 10px">
+            <input name="submit" value="Submit" type="submit" class="btn btn-primary">
+          </div>
+        </form>
+        <div style="text-align: center;">
+          <?php echo $product != null ? $product->display() : ""?>
+        </div>
       </div>
-    </div>
-  </body>
-</html>
+    </body>
+  </html>
